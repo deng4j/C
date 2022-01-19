9311c0e34d953a9d91ec5011973dc9f911894716
邓志鸿 <1353976012@qq.com>
2022-01-20 00:46:17 +0800

    day5

---
+++ vs/demo9_h/header.c
@@ -0,0 +1,7 @@
+
+/*测试一个头文件被引用两次*/
+#include"header.h"
+
+int add(int a, int b) {
+	return a + b;
+}
---
+++ vs/demo9_h/header.h
@@ -0,0 +1,9 @@
+
+//头文件标准方式，防止头文件重复编译
+#ifndef HEADER_H
+#define HEADER_H
+
+//把函数的使用范围拓展到这
+int add(int a, int b);
+
+#endif
