911645b4eee113657a049988ef123d0b12db905e
邓志鸿 <1353976012@qq.com>
2022-03-31 23:58:19 +0800

    练习

---
+++ vs/exercise/prime_number/conversion.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int mainFVGHBJ() {
+	//16进制转10进制
+	printf("%15d\n",0XABCDEF);
+	//10进制转8进制和16进制
+	printf("%#0o %#0X\n", 1234, 1234);
+	return 0;
+}
--- vs/exercise/prime_number/getchar.c
+++ vs/exercise/prime_number/getchar.c
@@ -1,5 +1,6 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
 
 
 /*
@@ -7,7 +8,7 @@ getchar()每次会去缓冲区去取一个字符
 scanf会将空间中换行符'\n'前的字符串全部取出，存入到地址中
 */
 
-int main() {
+int mainFGVH() {
 
 	int ch = 0;
 	int temp = 0;
@@ -30,6 +31,78 @@ int main() {
 		printf("%c\n", temp);
 		getchar(); //用于清理输入的换行符'\n'
 	}
+
+	return 0;
+}
+
+
+int mainTFYH() {
+	char x;
+	int y;
+	double z;
+	scanf_s("%c", &x);
+	scanf_s("%d",&y);
+	scanf_s("%lf",&z);
+
+	printf("%c %d %.6lf", x, y, z);
+	return 0;
+}
+
+
+int check(int n) {
+	if (n>=0&&n<=100)
+	{
+		return 1;
+	}
+	return 0;
+}
+int mainGHJG()
+{
+	int a, b, c;
+	printf("请输入:\n");
+	scanf("%d %d %d", &a,&b,&c);
+	if (check(a)&&check(b)&&check(c))
+	{
+		printf("score1=%d,score2=%d,score3=%d", a, b, c);
+	}
+	
+	return 0;
+}
+
+
+int mainCFTVGBHJ() {
+	char a[50];
+	printf("请输入:\n");
+	scanf("%[^\n]", a); //输入换行键才结束
+	printf("str:%s",a);
+}
+
+//除法+取模，获取数字
+int mainGUUGHIU() {
+	int a;
+	printf("请输入:\n");
+	scanf("%d", &a);
+
+	int y = a / 10000;
+	int m = (a / 100) % 100;
+	int d = a % 100;
+
+	printf("year=%04d\n month=%02d\n day=%02d\n", y,m,d);
+
+	return 0;
+}
+
+//使用异或交换数据
+int mainGBHJ() {
+	int a,b;
+	printf("请输入:\n");
+	scanf("a=%d,b=%d", &a,&b);
+
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+
+	printf("a=%d,b=%d", a,b);
 
 	return 0;
 }
