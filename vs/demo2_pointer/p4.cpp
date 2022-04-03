ff9094f962ca8585e9853aded5226d4e8141d39e
邓志鸿 <1353976012@qq.com>
2022-04-03 18:44:08 +0800

    练习

---
+++ vs/demo2_pointer/p4.cpp
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+//指针数组
+int mainHIK() {
+    int a = 45, b = 878, c = 211;
+    //定义一个指针数组
+    int* arr[3] = { &a, &b, &c };
+    //赋值
+    *arr[0] = 555;
+    //定义一个指向指针数组的指针
+    int** parr = arr;
+    printf("%d, %d, %d\n", *arr[0], *arr[1], *arr[2]);
+    printf("%d, %d, %d\n", **(parr + 0), **(parr + 1), **(parr + 2));
+    return 0;
+}
+
+
+//数组指针
+void mainHJK()
+{
+    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    int* p;
+    int(*q)[10];
+    p = arr;  //数组首元素地址
+    q = &arr;  //数组首地址
+
+    //（数组首元素地址的操作）数组元素的引用与表示, 以第0个元素为例
+    printf("************数组元素的7种基本表示方法*****\n");
+    printf("arr[0] = %d, 1[arr] = %d, *(arr+2) = %d\n", arr[0], 1[arr], *(arr + 2));
+    printf("p [0] = %d,  1[p]  = %d,  *(p+2)  = %d\n", p[0], 1[p], *(p + 2));
+    printf(" *((int*)q + 1) = %d\n", *((int*)q + 1));
+    printf("数组首元素的地址 = %p\n", p);
+    printf("数组首元素的地址加一后的地址及其值: %p, %d\n", p + 1, *(p + 1));
+    //（数组首地址的操作）指针指向的特殊改变
+    printf("数组首地址 = %p\n", q);
+    printf("数组首地址加一后的地址及其值：%p, %d\n", q + 1, *(int*)(q + 1));
+}
+
+
+//动态创建一维数组
+int mainFYGT()
+{
+
+    int n1, i;
+    int* array;
+    printf("请输入所要创建的一维动态数组的长度：\n");
+    scanf("%d", &n1);
+    array = (int*)calloc(n1, sizeof(int));
+
+    for (i = 0; i < n1; i++)
+    {
+        array[i] = i + 1;
+        printf("%d\t", array[i]);
+    }
+    //释放第一维指针 
+    free(array);
+    return 0;
+}
+
+//动态创建二维数组
+int main()
+{
+    int n1, n2;
+    int** array, i, j;
+    printf("请输入所要创建的动态数组的第一维长度：\n");
+    scanf("%d", &n1);
+    printf("请输入所要创建的动态数组的第二维长度：\n");
+    scanf("%d", &n2);
+
+    //第一维 
+    array = (int**)malloc(n1 * sizeof(int*)); 
+    for (i = 0; i < n1; i++)
+    {
+        //第二维 
+        array[i] = (int*)malloc(n2 * sizeof(int));
+    }
+    for (i = 0; i < n1; i++)
+    {
+
+        for (j = 0; j < n2; j++)
+        {
+            array[i][j] = i * n2 + j + 1;
+            printf("%d\t", array[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 0; i < n1; i++)
+    {
+        //释放第二维指针 
+        free(array[i]);
+    }
+    //释放第一维指针 
+    free(array);
+    return 0;
+}
--- vs/exercise/prime_number/getchar.c
+++ vs/exercise/prime_number/getchar.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
+#include "getchar.h"
 
 
 /*
@@ -92,6 +94,15 @@ int mainGUUGHIU() {
 	return 0;
 }
 
+int mainIUK() {
+	int a;
+	printf("请输入:\n");
+	scanf("%d", &a);
+
+	printf("%d", (a%100)/10);
+	return 0;
+}
+
 //使用异或交换数据
 int mainGBHJ() {
 	int a,b;
@@ -106,3 +117,111 @@ int mainGBHJ() {
 
 	return 0;
 }
+
+//空格
+int mainJOIJ() {
+	int a, b,c;
+	printf("请输入:\n");
+	scanf("%d %d %d", &a, &b, &c);
+
+	printf("%d%8d%8d", a, b, c);
+	return 0;
+}
+
+
+//进制相加
+int mainHHIU() {
+	int a, b,c;
+	printf("请输入:\n");
+	scanf("%x %o", &a, &b);
+	c = a + b;
+	printf("%d", c);
+	return 0;
+}
+
+
+int mainBHK() {
+	float a;
+	int b;
+	printf("请输入:\n");
+	scanf("%f", &a);
+	b = (int)a % 10;
+	
+	printf("%d", b);
+	return 0;
+}
+
+
+int mainHIIK() {
+	int a, b;
+	printf("请输入:\n");
+	scanf("%d %d", &a,&b);
+
+	printf("%d %d", (a/b),(a%b) );
+	return 0;
+}
+
+
+int mainUGYU() {
+	int a, b;
+	printf("请输入:\n");
+	scanf("%d %d", &a, &b);
+	b = (a + b) % 7;
+	a =b ==0?7:b;
+
+	printf("%d", a);
+	return 0;
+}
+
+//计算时分秒
+int mainHUI() {
+	int a,b,c;
+	printf("请输入:\n");
+	scanf("%d", &a);
+	b = a / 3600;
+	a = a - (b * 3600);
+	c = a / 60;
+	a = a - (c * 60);
+
+	printf("%d %d %d", b,c,a);
+	return 0;
+}
+
+
+//位运算
+int mainGUY() {
+	int n;
+	printf("请输入:\n");
+	scanf("%d", &n);
+
+	printf("%d", (1 <<n));
+	return 0;
+}
+
+//次方运算
+int mainHUJJJI() {
+	int age;
+	printf("请输入:\n");
+	scanf("%d", &age);
+
+	long seconds = 3.156 *pow(10,7) * age;
+
+	printf("%ld",seconds );
+	return 0;
+}
+
+
+int main() {
+	int n=0;
+	printf("请输入数据个数:\n");
+	scanf("%d", &n);
+	//int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+
+	}
+
+	printf("%d", n);
+	return 0;
+}
+
